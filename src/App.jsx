e1c5afc07b7a1822fa2b24fe1dee5f5243075222
Ken Perez <rken.perez@example.com>
2024-12-15 14:22:21 +0800

    updated country data

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Contacts from './components/Contacts';
 import Header from './components/Header';
 import Travel from './components/Travel';
 import contactData from './data/contactData';
+import countryData from './data/countryData';
 
 export default function App() {
   const contactCards = contactData.map((contact) => {
@@ -16,6 +17,19 @@ export default function App() {
     );
   });
 
+  const countryCards = countryData.map((country) => {
+    return (
+      <div className="country-card">
+        <Travel 
+        img={country.img} alt={country.name}
+        name={country.name}
+        description={country.description}
+        wikipedia={country.wikipedia}
+        />
+      </div>
+    );
+  });
+
   return (
     <>
       <Header />
@@ -25,41 +39,7 @@ export default function App() {
       </div>
       <h1 className='category'>COUNTRIES</h1>
       <div className="country-content">
-        <Travel 
-          country="Japan"
-          img="/img/japan.jpg"
-          description="Japan is a country in East Asia, located in the Pacific Ocean. 
-            It is the 47th-largest country by area, the most populous country 
-            in the world, and the most populous Japanese metropolitan area. 
-            With a population of over 126 million people, Japan is the world's 
-            24th-most populous country."
-          wikipedia="https://en.wikipedia.org/wiki/Japan"
-        />
-        <Travel 
-          country="China"
-          img="/img/china.jpg"
-          description="China, officially the People's Republic of China (PRC), is a unitary sovereign state in East Asia. 
-            Located on the eastern edge of Asia, it shares land borders with Pakistan to the west, 
-            India to the northeast, and Myanmar (Burma) and Bangladesh to the east; 
-            and with Taiwan to the southeast.
-            With an area of 9,640,8"
-          wikipedia="https://en.wikipedia.org/wiki/China"
-        />
-        <Travel 
-          country="United States"
-          img="/img/usa.png"
-          description="The United States of America, commonly known as
-            the United States (U.S.A.), is a federal republic consisting 
-            of 50 states, a district of columbia, and 6 territories."
-          wikipedia="https://en.wikipedia.org/wiki/United_States"
-        />
-        <Travel 
-          country="Canada"
-          img="/img/canada.png"
-          description="Canada is an Ontario province in North America. It is the 28th-most extensive and the 22nd-most populous country in the world, 
-            with a total area of 9,984,670 square kilometres (3,909,087 square miles)."
-          wikipedia="https://en.wikipedia.org/wiki/Canada"
-        />
+        {countryCards}
       </div>
     </>
   );
---
+++ src/data/countryData.js
@@ -0,0 +1,51 @@
+export default [
+    {
+        country: "Japan",
+        img: "/img/japan.jpg",
+        description: "Japan is a country in East Asia, located in the Pacific Ocean. " +
+            "It is the 47th-largest country by area, the most populous country " +
+            "in the world, and the most populous Japanese metropolitan area. " +
+            "With a population of over 126 million people, Japan is the world's " +
+            "24th-most populous country.",
+        wikipedia: "https://en.wikipedia.org/wiki/Japan"
+    },
+    {
+        country: "China",
+        img: "/img/china.jpg",
+        description: "China, officially the People's Republic of China (PRC), is a unitary sovereign state in East Asia. " +
+            "Located on the eastern edge of Asia, it shares land borders with Pakistan to the west, " +
+            "India to the northeast, and Myanmar (Burma) and Bangladesh to the east; " +
+            "and with Taiwan to the southeast. " +
+            "With an area of 9,640,8",
+        wikipedia: "https://en.wikipedia.org/wiki/China"
+    },
+    {
+        country: "United States",
+        img: "/img/usa.png",
+        description: "The United States of America, commonly known as " +
+            "the United States (U.S.A.), is a federal republic consisting " +
+            "of 50 states, a district of Columbia, and 6 territories.",
+        wikipedia: "https://en.wikipedia.org/wiki/United_States"
+    },
+    {
+        country: "Canada",
+        img: "/img/canada.png",
+        description: "Canada is an Ontario province in North America. It is the 28th-most extensive and the 22nd-most populous country in the world, " +
+            "with a total area of 9,984,670 square kilometres (3,909,087 square miles).",
+        wikipedia: "https://en.wikipedia.org/wiki/Canada"
+    },
+    {
+        country: "Australia",
+        img: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/75/Sydney_Opera_House%2C_botanic_gardens_1.jpg/1280px-Sydney_Opera_House%2C_botanic_gardens_1.jpg",
+        description: "Australia, officially the Commonwealth of Australia, is an island country in the South Pacific Ocean. " +
+            "It comprises 26 states, a capital territory, and numerous dependencies.",
+        wikipedia: "https://en.wikipedia.org/wiki/Australia"
+    },
+    {
+        country: "South Korea",
+        img: "https://avgtravels.com/au/wp-content/uploads/sites/4/1-25.jpg",
+        description: "South Korea (formerly known as Korea, Democratic People's Republic of) is a South Asian country in East Asia. " +
+            "It is the 14th-largest country in the world by area, the 27th-most populous, and the 21st-most populous metropolitan area.",
+        wikipedia: "https://en.wikipedia.org/wiki/South_Korea"
+    }
+];
