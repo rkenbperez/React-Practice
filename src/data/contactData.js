df630eeee08eee55ddcfc1a319f55e6bf881061f
Ken Perez <juicyken068@gmail.com>
2024-12-16 20:45:34 +0800

    added new people

--- src/data/contactData.js
+++ src/data/contactData.js
@@ -46,5 +46,17 @@ export default [
          img:"https://upload.wikimedia.org/wikipedia/commons/thumb/c/c7/Paul_Graham_TechCrunch_Disrupt_New_York_May_2011.jpg/800px-Paul_Graham_TechCrunch_Disrupt_New_York_May_2011.jpg",
          number:"+7 890 123 4567",
          location:"New York, NY"
+     },
+     {
+        name:"Noah Fulgado",
+        img:"https://scontent.fmnl19-1.fna.fbcdn.net/v/t39.30808-1/462972243_8283805855020776_124165592294373836_n.jpg?stp=c10.0.769.769a_dst-jpg_s200x200_tt6&_nc_cat=108&ccb=1-7&_nc_sid=0ecb9b&_nc_ohc=O4XGO6XqRl8Q7kNvgHv6T05&_nc_zt=24&_nc_ht=scontent.fmnl19-1.fna&_nc_gid=AsFTvZfZZfUzAZSZtUL8IFO&oh=00_AYD7o7s1kGMFY93UxQFaHm3ihs3NBGyks0J9kOErDOWbEw&oe=6766075E",
+        number:"+63 999 123 4567",
+        location:"Bolinao, Philippines"
+     },
+     {
+        name:"Ken Perez",
+        img:"https://scontent.fmnl19-1.fna.fbcdn.net/v/t39.30808-6/449832239_1545133539689850_5825015355617718052_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=LIACYrGx4JEQ7kNvgG0Ds38&_nc_zt=23&_nc_ht=scontent.fmnl19-1.fna&_nc_gid=AtPdrQH4PVuXe_8L30FCA6t&oh=00_AYDw5yVIHIGVgey1Vo_q44K2fcR6UIAhCtPRN0eCpd5U5A&oe=6765E594",
+        number:"+63 945 521 6331",
+        location:"Dasmarinas, Philippines"
      }
 ]
